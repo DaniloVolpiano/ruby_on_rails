b4530c38cbb6a37fb22791ff2282c9d50639fe54
DaniloVolpiano <danilo.volpiano@gmail.com>
2020-07-29 22:27:50 -0300

    Update project files

--- workspace/modulo_04/time_to_answer/config/routes.rb
+++ workspace/modulo_04/time_to_answer/config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   namespace :site do
     get 'welcome/index'
   end
-  namespace :profiles_backoffice do
+  namespace :users_backoffice do
     get 'welcome/index'
   end
   namespace :admins_backoffice do
     get 'welcome/index'
   end
+
   devise_for :admins
+  devise_for :users
   
   get 'inicio', to: 'site/welcome#index'
 
