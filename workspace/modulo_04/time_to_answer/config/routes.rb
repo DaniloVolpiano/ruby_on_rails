e676f0e234d4223260cbfed2733de05639e99596
DaniloVolpiano <danilo.volpiano@gmail.com>
2020-08-19 16:23:21 -0300

    Add search bar

--- workspace/modulo_04/time_to_answer/config/routes.rb
+++ workspace/modulo_04/time_to_answer/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :site do
     get 'welcome/index'
+    get 'search', to: 'search#questions'
   end
   namespace :users_backoffice do
     get 'welcome/index'
@@ -16,7 +17,6 @@ Rails.application.routes.draw do
   devise_for :users
  
   get 'inicio', to: 'site/welcome#index'
-
   root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
