2790d5494727b2552146c1b85c255382082601d2
DaniloVolpiano <danilo.volpiano@gmail.com>
2020-09-14 20:00:40 -0300

    Create API

--- workspace/modulo_04/time_to_answer/db/schema.rb
+++ workspace/modulo_04/time_to_answer/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_203428) do
+ActiveRecord::Schema.define(version: 2020_09_14_222312) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_203428) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "zip_code"
     t.index ["user_id"], name: "index_user_profiles_on_user_id"
   end
 
